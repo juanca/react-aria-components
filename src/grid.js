5af5412bed8c8c7fd8826d72d6301cafd8ce8d3a
Juan Carlos Medina <juanca.med@gmail.com>
2018-05-25 16:03:14 -0700

    Extract row as a Grid component
    A Grid component has many cells. At some point, this grid needs to be part of a Node since a tree is a node and a list of nodes.

---
+++ src/grid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import styles from './grid.css';
+
+export default function Grid(props) {
+  return (
+    <div className={styles.container}>
+      <div className={styles.c0}>B0</div>
+      <div className={styles.c1}>B0</div>
+      <div className={styles.c2}>B0</div>
+      <div className={styles.c3}>B0</div>
+      <div className={styles.c4}>B0</div>
+    </div>
+  );
+};
