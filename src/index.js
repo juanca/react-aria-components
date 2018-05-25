36be943a8bec0c1a04e9eaf30268b990f24b8da0
Juan Carlos Medina <juanca.med@gmail.com>
2018-05-25 13:36:43 -0700

    html-webpack-plugin init

---
+++ src/index.js
@@ -0,0 +1,1 @@
+document.querySelector('body').appendChild(document.createTextNode('Hello world!'));
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: {
+    rules: [{
+      test: /\.html$/,
+      use: [{
+        loader: 'html-loader',
+      }],
+    }],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ]
+};
