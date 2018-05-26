485e0b597ec3d5259fdddb70074594daf260bf1c
Juan Carlos Medina <juanca.med@gmail.com>
2018-05-25 17:28:58 -0700

    Expand column definition to many lines for easier editing and git history compatibility

--- src/index.js
+++ src/index.js
@@ -4,13 +4,28 @@ import { render } from 'react-dom';
 import TreeGrid from './tree-grid.js';
 import styles from './index.css';
 
-const columns = [
-  { component: 'HEAD0', dataKey: 'b0', id: 'c0', className: styles.c0, labelClassName: styles.hc0 },
-  { component: 'HEAD1', dataKey: 'b1', id: 'c1', className: styles.c1 },
-  { component: 'HEAD2', dataKey: 'b2', id: 'c2', className: styles.c2 },
-  { component: 'HEAD3', dataKey: 'b3', id: 'c3', className: styles.c3 },
-  { component: 'HEAD4', dataKey: 'b4', id: 'c4', className: styles.c4 },
-];
+const columns = [{
+  id: 'c0',
+  className: styles.c0,
+  component: 'HEAD0',
+  labelClassName: styles.hc0
+}, {
+  id: 'c1',
+  className: styles.c1
+  component: 'HEAD1',
+}, {
+  id: 'c2',
+  className: styles.c2
+  component: 'HEAD2',
+}, {
+  id: 'c3',
+  className: styles.c3
+  component: 'HEAD3',
+}, {
+  id: 'c4',
+  className: styles.c4
+  component: 'HEAD4',
+}];
 
 render((
   <React.Fragment>
