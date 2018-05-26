eb796cad29a35037bcf17fc9539f2c7cf7af7afe
Juan Carlos Medina <juanca.med@gmail.com>
2018-05-25 17:36:54 -0700

    Add missing trailing commas

--- src/index.js
+++ src/index.js
@@ -8,21 +8,21 @@ const columns = [{
   className: styles.c0,
   id: 'c0',
   labelNode: 'HEAD0',
-  labelClassName: styles.hc0
+  labelClassName: styles.hc0,
 }, {
-  className: styles.c1
+  className: styles.c1,
   id: 'c1',
   labelNode: 'HEAD1',
 }, {
-  className: styles.c2
+  className: styles.c2,
   id: 'c2',
   labelNode: 'HEAD2',
 }, {
-  className: styles.c3
+  className: styles.c3,
   id: 'c3',
   labelNode: 'HEAD3',
 }, {
-  className: styles.c4
+  className: styles.c4,
   id: 'c4',
   labelNode: 'HEAD4',
 }];
