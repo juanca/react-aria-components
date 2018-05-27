6092594a81dc6c1f44c373cd4f58ad2ee2295b7a
Juan Carlos Medina <juanca.med@gmail.com>
2018-05-27 15:22:11 -0700

    Dynamically create column set definition and data
    Helps with testing new features for big data tables

--- src/index.js
+++ src/index.js
@@ -4,44 +4,22 @@ import { render } from 'react-dom';
 import TreeGrid from './tree-grid.js';
 import styles from './index.css';
 
-const columns = [{
-  className: styles.c0,
-  id: 'c0',
-  labelNode: 'HEAD0',
-  labelClassName: styles.hc0,
-  renderNode: (datum => datum.b0),
-}, {
-  className: styles.c1,
-  id: 'c1',
-  labelNode: 'HEAD1',
-  renderNode: (datum => datum.b1),
-}, {
-  className: styles.c2,
-  id: 'c2',
-  labelNode: 'HEAD2',
-  renderNode: (datum => datum.b2),
-}, {
-  className: styles.c3,
-  id: 'c3',
-  labelNode: 'HEAD3',
-  renderNode: (datum => datum.b3),
-}, {
-  className: styles.c4,
-  id: 'c4',
-  labelNode: 'HEAD4',
-  renderNode: (datum => datum.b4),
-}];
+const numberOfColumns = 20;
+const numberOfRows = 20;
 
-const data = [
-  { b0: 'B0', b1: 'B1', b2: 'B2', b3: 'B3', b4: 'B4', id: 0 },
-  { b0: 'B0', b1: 'B1', b2: 'B2', b3: 'B3', b4: 'B4', id: 1 },
-  { b0: 'B0', b1: 'B1', b2: 'B2', b3: 'B3', b4: 'B4', id: 2 },
-  { b0: 'B0', b1: 'B1', b2: 'B2', b3: 'B3', b4: 'B4', id: 3 },
-  { b0: 'B0', b1: 'B1', b2: 'B2', b3: 'B3', b4: 'B4', id: 4 },
-  { b0: 'B0', b1: 'B1', b2: 'B2', b3: 'B3', b4: 'B4', id: 5 },
-  { b0: 'B0', b1: 'B1', b2: 'B2', b3: 'B3', b4: 'B4', id: 6 },
-  { b0: 'B0', b1: 'B1', b2: 'B2', b3: 'B3', b4: 'B4', id: 7 },
-];
+const columns = new Array(numberOfColumns).fill(0).map((_, index) => ({
+  className: styles[`c${index % 5}`],
+  id: `c${index}`,
+  labelNode: `HEAD #${index % 5}`,
+  labelClassName: index === 0 ? styles.hc0 : undefined,
+  renderNode: (datum => datum[`b${index}`]),
+}));
+
+const data = new Array(numberOfRows).fill(0).map((_, rIndex) =>
+  new Array(numberOfColumns).fill(0).reduce((datum, _, cIndex) => Object.assign(datum, {
+    [`b${cIndex}`]: `BCELL-${rIndex}-${cIndex}`,
+  }), { id: rIndex })
+);
 
 render((
   <React.Fragment>
