17121c81834993a3dfa471fbd8c2d0e0bb143e7a
Juan Carlos Medina <juanca.med@gmail.com>
2018-06-14 15:31:52 -0700

    Replace dynamically generated data with declaratively generated data
    Not as much data as before but it is nicer looking and can provide more insight into structuring the properties interface API.

--- src/examples/grid-example.js
+++ src/examples/grid-example.js
@@ -1,24 +1,43 @@
+import faker from 'faker';
 import React from 'react';
 
 import Grid from '../grid.js';
 import styles from './grid-example.css';
 
-const numberOfColumns = 20;
-const numberOfRows = 200;
+function get(key) {
+  return function columnCell(datum) {
+    return datum[key];
+  }
+}
 
-const columns = new Array(numberOfColumns).fill(0).map((_, index) => ({
-  id: `c${index}`,
-  columnCell: (datum => datum[`b${index}`]),
-  columnClassName: index === 0 ? styles.c0 : styles[`c${(index % 4) + 1}`],
-  columnHeaderCell: <span>{`HEAD #${index % 5}`}</span>,
-  columnHeaderClassName: index === 0 ? styles.hc0 : undefined,
-}));
+const columns = [
+  { id: 1, columnCell: get('findName'), columnClassName: styles['frozen-column'], columnHeaderCell: 'Name', columnHeaderClassName: undefined },
+  { id: 2, columnCell: get('jobTitle'), columnClassName: undefined, columnHeaderCell: 'Job Title', columnHeaderClassName: undefined },
+  { id: 3, columnCell: get('jobDescriptor'), columnClassName: undefined, columnHeaderCell: 'Job Description', columnHeaderClassName: undefined },
+];
 
-const data = new Array(numberOfRows).fill(0).map((_, rIndex) =>
-  new Array(numberOfColumns).fill(0).reduce((datum, _, cIndex) => Object.assign(datum, {
-    [`b${cIndex}`]: `BCELL-${rIndex}-${cIndex}`,
-  }), { id: rIndex })
-);
+const data = [
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+  { findName: faker.name.findName(), jobTitle: faker.name.jobTitle(), jobDescriptor: faker.name.jobDescriptor() },
+]
 
 export default function GridExample() {
   return (
