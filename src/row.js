80fa582345cee6810ddf16abef8bcb477dfa54c3
Juan Carlos Medina <juanca.med@gmail.com>
2018-06-13 17:50:30 -0700

    Remove unused code from tree grid half-baked implementation (that has been removed since)

--- src/row.js
+++ src/row.js
@@ -6,25 +6,18 @@ import PropTypeColumns from './prop-types/columns.js';
 import PropTypeDatum from './prop-types/datum.js';
 import styles from './row.css'
 
-const subtreeLength = 2;
-const subtreeChildren = new Array(subtreeLength).fill(0).map((_, index) => index);
-
 export default function Row(props) {
   return (
-    <React.Fragment>
-      <Grid
-        key={props.id}
-      >
-        {props.columns.map(column =>
-          <GridCell
-            className={column.className}
-            key={column.id}
-          >
-            {column.renderNode(props.datum)}
-          </GridCell>
-        )}
-      </Grid>
-    </React.Fragment>
+    <Grid>
+      {props.columns.map(column =>
+        <GridCell
+          className={column.className}
+          key={column.id}
+        >
+          {column.renderNode(props.datum)}
+        </GridCell>
+      )}
+    </Grid>
   );
 };
 
