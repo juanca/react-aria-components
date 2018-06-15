ccfe675e714a67c5d7a66a26f22c81e821ff8181
Juan Carlos Medina <juanca@mavenlink.com>
2018-06-15 14:58:13 -0700

    Remove unused drag and drop code and testing utilities

--- src/row.js
+++ src/row.js
@@ -1,17 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import eventHandlersFactory from './utils/event-handlers-factory.js';
 import styles from './row.css'
 
 export default function Row(props) {
   return (
-    <div
-      className={props.className}
-      draggable
-      role="row"
-      {...eventHandlersFactory('Grid', ['Click', 'Drag'])}
-    >
+    <div className={props.className} role="row">
       {props.children}
     </div>
   );
