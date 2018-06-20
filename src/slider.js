aee58f6394b38a5975fad895bda6ec870446f18e
Juan Carlos Medina <juanca.med@gmail.com>
2018-06-19 19:13:45 -0700

    Implement wireframe for Slider

--- src/slider.js
+++ src/slider.js
@@ -1,12 +1,27 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import styles from './slider.css';
+
 export default function Slider(props) {
   return (
-    <React.Fragment>
-      {props.children}
-    </React.Fragment>
+    <div className={styles.rail}>
+      <div
+        aria-valuemin={props.valueMin}
+        aria-valuenow={props.valueNow}
+        aria-valuemax={props.valueMax}
+        className={styles.thumb}
+        role="slider"
+        tabindex="0"
+      />
+    </div>
   );
+};
+
+Slider.defaultProps = {
+  valueMin: 0,
+  valueNow: 0,
+  valueMax: 1,
 };
 
 Slider.propTypes = {
