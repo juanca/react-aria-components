b99cabb3b02a0d33d94b25d8af33f7979215381a
Andrew Huth <andrew@huth.me>
2018-07-04 18:23:48 -0700

    Move tabs into their own directory

---
+++ src/tabs/index.js
@@ -0,0 +1,4 @@
+export { TabList  as TabList} from './tab-list.js';
+export { TabPanels as TabPanels } from './tab-panels.js';
+export { Tab as Tab } from './tab.js';
+export { Tabs as Tabs } from './tabs.js';
