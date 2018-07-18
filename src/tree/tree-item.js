fd0d2c3164bb0ccdca5663f03882627c3998d6e2
Andrew Huth <andrew@huth.me>
2018-07-18 09:22:52 -0700

    Handle the open state for sub trees as a prop

--- src/tree/tree-item.js
+++ src/tree/tree-item.js
@@ -3,17 +3,12 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default class TreeItem extends React.Component {
-  constructor() {
-    super();
-    this.state = { open: false };
-  }
-
   render() {
-    const { children, title } = this.props;
+    const { children, open, title } = this.props;
 
     return (
       <li
-        aria-expanded={children ? this.state.open : undefined}
+        aria-expanded={children ? open : undefined}
         role="treeitem"
         tabIndex={undefined}
       >
@@ -30,9 +25,11 @@ export default class TreeItem extends React.Component {
 
 TreeItem.propTypes = {
   children: PropTypes.node,
+  open: PropTypes.bool,
   title: PropTypes.node.isRequired,
 };
 
 TreeItem.defaultProps = {
   children: undefined,
+  open: undefined,
 };
