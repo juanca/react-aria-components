e6cfb50f54ecedfc88260e27d9e13a7435b62ff4
Andrew Huth <andrew@huth.me>
2018-07-18 14:15:51 -0700

    Lint

--- src/tree/tree-item.js
+++ src/tree/tree-item.js
@@ -1,39 +1,40 @@
-/* eslint-disable */
 import PropTypes from 'prop-types';
 import React from 'react';
 import ActiveIdContext from './active-id-context';
 
-export default class TreeItem extends React.Component {
-  render() {
-    const { children, id, open, title } = this.props;
+export default function TreeItem({
+  children,
+  id,
+  open,
+  title,
+}) {
+  return (
+    <ActiveIdContext.Consumer>
+      {(activeId) => {
+        const active = activeId === id;
 
-    return (
-      <ActiveIdContext.Consumer>
-        {(activeId) => {
-          const active = activeId === id;
-
-          return (
-            <li
-              aria-expanded={children ? open : undefined}
-              role="treeitem"
-              tabIndex={active ? '0' : undefined}
-            >
-              <span>{title}</span>
-              {children && (
-                <ul role="group">
-                  {children}
-                </ul>
-              )}
-            </li>
-          );
-        }}
-      </ActiveIdContext.Consumer>
-    );
-  }
+        return (
+          <li
+            aria-expanded={children ? open : undefined}
+            role="treeitem"
+            tabIndex={active ? '0' : undefined}
+          >
+            <span>
+              {title}
+            </span>
+            {children && (
+              <ul role="group">
+                {children}
+              </ul>
+            )}
+          </li>
+        );
+      }}
+    </ActiveIdContext.Consumer>
+  );
 }
 
 TreeItem.propTypes = {
-  // activeId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   children: PropTypes.node,
   id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   open: PropTypes.bool,
--- src/tree/tree.js
+++ src/tree/tree.js
@@ -1,33 +1,34 @@
-/* eslint-disable */
 import PropTypes from 'prop-types';
 import React from 'react';
 import ActiveIdContext from './active-id-context';
 
-export default class Tree extends React.Component {
-  render() {
-    const {
-      activeId,
-      'aria-label': ariaLabel,
-      'aria-labelledby': ariaLabelledBy,
-      children
-    } = this.props;
-
-    return (
-      <ul
-        aria-label={ariaLabel}
-        aria-labelledby={ariaLabelledBy}
-        role="tree"
-      >
-        <ActiveIdContext.Provider value={activeId}>
-          {children}
-        </ActiveIdContext.Provider>
-      </ul>
-    );
-  }
+export default function Tree({
+  activeId,
+  'aria-label': ariaLabel,
+  'aria-labelledby': ariaLabelledBy,
+  children,
+}) {
+  return (
+    <ul
+      aria-label={ariaLabel}
+      aria-labelledby={ariaLabelledBy}
+      role="tree"
+    >
+      <ActiveIdContext.Provider value={activeId}>
+        {children}
+      </ActiveIdContext.Provider>
+    </ul>
+  );
 }
 
 Tree.propTypes = {
+  activeId: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   'aria-label': PropTypes.string,
   'aria-labelledby': PropTypes.string,
   children: PropTypes.node.isRequired,
+};
+
+Tree.defaultProps = {
+  'aria-label': undefined,
+  'aria-labelledby': undefined,
 };
