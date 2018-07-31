d2cb39e03950fb737127450e9b634a7769a18527
Juan Carlos Medina <juanca.med@gmail.com>
2018-07-31 12:05:02 -0700

    Extract React fragment from examples entry point into own component for SSR compatibility
    This new file serves as the index of all React Components that will need to be compiled for hypernova to consume.

--- src/examples/index.js
+++ src/examples/index.js
@@ -4,48 +4,8 @@ import 'babel-polyfill'; // eslint-disable-line import/no-extraneous-dependencie
 import React from 'react';
 import { render } from 'react-dom';
 
-import Example from './example.js';
-import GridExample from './grid-example.js';
-import TabsExample from './tabs-example.js';
+import Examples from './examples.js';
 
 setTimeout(() => {
-  render((
-    <React.Fragment>
-      <header>
-        <h1>
-          React ARIA Components
-        </h1>
-      </header>
-      <Example title="Table of Contents">
-        <nav>
-          <p>
-            List of WAI-ARIA components implemented in React:
-          </p>
-          <ol>
-            <li>
-              Grid
-            </li>
-            <li>
-              Tabs
-            </li>
-          </ol>
-        </nav>
-      </Example>
-      <main>
-        <GridExample />
-        <TabsExample />
-      </main>
-      <footer>
-        <address>
-          <a href="https://github.com/juanca/react-aria-components">
-            Github repository
-          </a>
-          <br />
-          <a href="https://npmjs.com/package/react-aria-components">
-            NPM package
-          </a>
-        </address>
-      </footer>
-    </React.Fragment>
-  ), window.document.getElementById('page'));
+  render(<Examples />, window.document.getElementById('page'));
 }, 100);
