c5a2ad5eb94710278bd1069b484c60fdd1949dc3
Juan Carlos Medina <juanca.med@gmail.com>
2018-11-18 15:30:25 -0800

    react tests init

--- src/cursor/index.js
+++ src/cursor/index.js
@@ -1,1 +1,11 @@
-export default {};
+import React from 'react';
+
+export default class Cursor extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.children()}
+      </div>
+    );
+  }
+};
--- src/cursor/index.test.js
+++ src/cursor/index.test.js
@@ -1,5 +1,17 @@
-import cursor from './index.js';
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
 
-test('is an object', () => {
-  expect(cursor).toEqual({});
+import Cursor from './index.js';
+
+test('is a component', () => {
+  const renderer = TestRenderer.create(
+      <Cursor>
+        {() => (
+          'Hello world!'
+        )}
+      </Cursor>
+  );
+
+  expect(renderer.toJSON().children.length).toEqual(1);
+  expect(renderer.toJSON().children[0]).toEqual('Hello world!');
 });
