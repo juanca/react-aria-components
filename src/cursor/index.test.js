30cbe4e12edc1666907ae3eed15b76c5841ce4f8
Juan Carlos Medina <juanca.med@gmail.com>
2018-11-18 16:34:00 -0800

    Hoist render method for tests

--- src/cursor/index.test.js
+++ src/cursor/index.test.js
@@ -3,16 +3,20 @@ import TestRenderer from 'react-test-renderer';
 
 import Cursor from './index.js';
 
-test('renders the default state', () => {
-  const renderer = TestRenderer.create(
-      <Cursor>
-        {(position) => (
-          `Current position: ${position}`
-        )}
-      </Cursor>
+function render() {
+  return TestRenderer.create(
+    <Cursor>
+      {(position) => (
+        `Current position: ${position}`
+      )}
+    </Cursor>
   );
+}
 
+test('renders the default state', () => {
+  const renderer = render();
   const root = renderer.toJSON();
+
   expect(root.children.length).toEqual(1);
   expect(root.children[0]).toEqual('Current position: -1');
 });
