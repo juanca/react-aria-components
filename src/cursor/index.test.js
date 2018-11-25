4ae7fd9ee7a85913f1ad3c333862ab2c7c574b81
Juan Carlos Medina <juanca.med@gmail.com>
2018-11-24 22:18:35 -0800

    Fill in `preventDefault` on synthetic event

--- src/cursor/index.test.js
+++ src/cursor/index.test.js
@@ -31,39 +31,39 @@ test('renders the default state', () => {
 test('increments its position on the x-axis', () => {
   const renderer = render();
 
-  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowRight' });
+  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowRight', preventDefault: jest.fn() });
   expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: 0, -1');
 
-  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowRight' });
+  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowRight', preventDefault: jest.fn() });
   expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: 1, -1');
 });
 
 test('decrements its position on the x-axis', () => {
   const renderer = render();
 
-  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowLeft' });
+  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowLeft', preventDefault: jest.fn() });
   expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -2, -1');
 
-  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowLeft' });
+  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowLeft', preventDefault: jest.fn() });
   expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -3, -1');
 });
 
 test('increments its position on the y-axis', () => {
   const renderer = render();
 
-  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowDown' });
+  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowDown', preventDefault: jest.fn() });
   expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -1, 0');
 
-  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowDown' });
+  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowDown', preventDefault: jest.fn() });
   expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -1, 1');
 });
 
 test('decrements its position on the y-axis', () => {
   const renderer = render();
 
-  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowUp' });
+  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowUp', preventDefault: jest.fn() });
   expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -1, -2');
 
-  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowUp' });
+  renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowUp', preventDefault: jest.fn()});
   expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -1, -3');
 });
