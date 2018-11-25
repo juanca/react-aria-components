3b9491bb8dcb104b636a7e1616417f50cc8651ce
Juan Carlos Medina <juanca.med@gmail.com>
2018-11-24 18:40:33 -0800

    Focus cell when it becomes active
    It might be better to check that the component transitioned into being active but this will do for now.

--- src/grid/grid-cell.js
+++ src/grid/grid-cell.js
@@ -3,11 +3,25 @@ import React from 'react';
 export default class Cell extends React.Component {
   constructor(props) {
     super(props);
+
+    this.nodeRef = React.createRef();
+  }
+
+  componentDidMount() {
+    if (this.props.active) {
+      this.nodeRef.current.focus();
+    }
+  }
+
+  componentDidUpdate() {
+    if (this.props.active) {
+      this.nodeRef.current.focus();
+    }
   }
 
   render() {
     return (
-      <span>
+      <span ref={this.nodeRef}>
         {this.props.active ? 'cell!' : 'cell'}
       </span>
     );
