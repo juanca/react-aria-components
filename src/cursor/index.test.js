4be7a1f6ad675ff8c02502f25b90818393f0f025
Juan Carlos Medina <juanca.med@gmail.com>
2018-11-24 22:20:33 -0800

    Update specs to reflect auto-index correction when navigating a Cursor
    Also, looks like the tabIndex on Cursor is now a Number.

--- src/cursor/index.test.js
+++ src/cursor/index.test.js
@@ -17,7 +17,7 @@ test('is receives keyboard events', () => {
   const renderer = render();
   const root = renderer.toJSON();
 
-  expect(root.props.tabIndex).toEqual('0');
+  expect(root.props.tabIndex).toEqual(0);
 });
 
 test('renders the default state', () => {
@@ -32,38 +32,38 @@ test('increments its position on the x-axis', () => {
   const renderer = render();
 
   renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowRight', preventDefault: jest.fn() });
-  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: 0, -1');
+  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: 0, 0');
 
   renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowRight', preventDefault: jest.fn() });
-  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: 1, -1');
+  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: 1, 0');
 });
 
 test('decrements its position on the x-axis', () => {
   const renderer = render();
 
   renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowLeft', preventDefault: jest.fn() });
-  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -2, -1');
+  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -2, 0');
 
   renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowLeft', preventDefault: jest.fn() });
-  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -3, -1');
+  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -3, 0');
 });
 
 test('increments its position on the y-axis', () => {
   const renderer = render();
 
   renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowDown', preventDefault: jest.fn() });
-  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -1, 0');
+  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: 0, 0');
 
   renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowDown', preventDefault: jest.fn() });
-  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -1, 1');
+  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: 0, 1');
 });
 
 test('decrements its position on the y-axis', () => {
   const renderer = render();
 
   renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowUp', preventDefault: jest.fn() });
-  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -1, -2');
+  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: 0, -2');
 
   renderer.toTree().rendered.props.onKeyDown({ key: 'ArrowUp', preventDefault: jest.fn()});
-  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: -1, -3');
+  expect(renderer.toTree().rendered.rendered[0]).toEqual('Current position: 0, -3');
 });
