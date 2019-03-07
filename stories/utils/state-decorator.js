9afdca1df2f56b35e36ce891d15408ddfbdd49be
Andrew Huth <andrew@huth.me>
2019-03-07 15:16:49 -0800

    Remove class properties syntax
    node doesn't understand it, and we're not transforming it.

--- stories/utils/state-decorator.js
+++ stories/utils/state-decorator.js
@@ -2,18 +2,13 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 export default class StateDecorator extends React.Component {
-  static propTypes = {
-    initialState: PropTypes.object, // eslint-disable-line react/forbid-prop-types
-    storyFn: PropTypes.func.isRequired,
-  };
+  constructor(props) {
+    super(props);
+    this.state = props.initialState;
+    this.handleSetState = this.handleSetState.bind(this);
+  }
 
-  static defaultProps = {
-    initialState: {},
-  };
-
-  state = this.props.initialState;
-
-  handleSetState = (...args) => {
+  handleSetState(...args) {
     this.setState(...args);
   }
 
@@ -25,3 +20,12 @@ export default class StateDecorator extends React.Component {
     });
   }
 }
+
+StateDecorator.propTypes = {
+  initialState: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  storyFn: PropTypes.func.isRequired,
+};
+
+StateDecorator.defaultProps = {
+  initialState: {},
+};
