5e832ed3890cab4b0d6fcf9f254375fbf3d4bf02
Andrew Huth <andrew@huth.me>
2019-03-07 14:50:17 -0800

    Move setup for Grid story into the story itself
    Hopefully when we're showing the story source this will make it easier to see how to use the components.

--- stories/grid.js
+++ stories/grid.js
@@ -5,26 +5,28 @@ import DataGrid from '../src/grid/data-grid.js';
 import GridRow from '../src/grid/grid-row.js';
 import GridCell from '../src/grid/grid-cell.js';
 
-const rows = 10;
-const columns = 10;
-
-const gridRefs = Array(rows).fill(undefined).map(() => (
-  Array(columns).fill(undefined).map(() => (
-    React.createRef()
-  ))
-));
-
 storiesOf('Grid', module)
-  .add('default', () => (
-    <DataGrid refs={gridRefs}>
-      {Array(rows).fill(undefined).map((row, y) => (
-        <GridRow key={y} cellRefs={gridRefs[y]} index={y}>
-          {Array(columns).fill(undefined).map((column, x) => (
-            <GridCell key={x} cellRef={gridRefs[y][x]}>
-              {active => (active ? 'cell!' : 'cell')}
-            </GridCell>
-          ))}
-        </GridRow>
-      ))}
-    </DataGrid>
-  ));
+  .add('default', () => {
+    const rows = 10;
+    const columns = 10;
+
+    const gridRefs = Array(rows).fill(undefined).map(() => (
+      Array(columns).fill(undefined).map(() => (
+        React.createRef()
+      ))
+    ));
+
+    return (
+      <DataGrid refs={gridRefs}>
+        {Array(rows).fill(undefined).map((row, y) => (
+          <GridRow key={y} cellRefs={gridRefs[y]} index={y}>
+            {Array(columns).fill(undefined).map((column, x) => (
+              <GridCell key={x} cellRef={gridRefs[y][x]}>
+                {active => (active ? 'cell!' : 'cell')}
+              </GridCell>
+            ))}
+          </GridRow>
+        ))}
+      </DataGrid>
+    )
+  });
