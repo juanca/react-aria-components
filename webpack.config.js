ad50c78e238bfd7e95bbf4e0a85880078a351b18
Juan Carlos Medina <juanca.med@gmail.com>
2019-06-15 12:12:03 -0700

    More dynamic configuration!
    I wonder if I will come to regret this. Only time will tell.

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,35 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 // Hoisted entries map to easily generate HtmlWebPackPlugin configurations
 const entries = {
-  accordions: './pages/accordions/index.js',
-  index: './pages/index.js',
-  grids: './pages/grids/index.js',
-  tabs: './pages/tabs/index.js',
+  accordions: {
+    entry: './pages/accordions/index.js',
+    publicPath: './accordions/index.html',
+    title: 'Accordion Examples - React WAI-ARIA Components',
+  },
+  grids: {
+    entry: './pages/grids/index.js',
+    publicPath: './grids/index.html',
+    title: 'Grid Examples - React WAI-ARIA Components',
+  },
+  index: {
+    entry: './pages/index.js',
+    publicPath: './index.html',
+    title: 'React WAI-ARIA Components',
+  },
+  tabs: {
+    entry: './pages/tabs/index.js',
+    publicPath: './tabs/index.html',
+    title: 'Tabs Examples - React WAI-ARIA Components',
+  },
 };
 
 module.exports = {
   devServer: {
     contentBase: './dist',
   },
-  entry: entries,
+  entry: Object.keys(entries).reduce((config, key) => (Object.assign(config, {
+    [key]: entries[key].entry,
+  })), {}),
   mode: 'development',
   module: {
     rules: [{
@@ -46,7 +64,8 @@ module.exports = {
     }),
   ].concat(Object.keys(entries).map(key => new HtmlWebPackPlugin({
     chunks: [key],
-    filename: key === 'index' ? './index.html' : `./${key}/index.html`,
-    template: key === 'index' ? './pages/index.html' : `./pages/${key}/index.html`,
+    filename: entries[key].publicPath,
+    template: './pages/index.html',
+    title: entries[key].title,
   }))),
 };
