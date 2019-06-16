5b340492190e061c905637f8d85314a741873c68
Juan Carlos Medina <juanca.med@gmail.com>
2019-06-16 12:23:04 -0700

    Implement toggle on dropdown button

--- pages/listboxes/example-2.js
+++ pages/listboxes/example-2.js
@@ -1,24 +1,41 @@
-/* eslint-disable react/no-array-index-key */
 import React from 'react';
 
 import Example from '../example.js';
 import styles from './example-2.css';
 
-export default function ListboxExample1() {
-  return (
-    <Example title="Collapsible Dropdown Listbox Example">
-      <div className={styles.container}>
-        <span>Choose an element:</span>
-        <div className={styles.wrapper}>
-          <button
-            aria-haspopup="listbox"
-            className={styles.button}
-            type="button"
-          >
-            Neptunium
-          </button>
+export default class ListboxExample2 extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: false,
+    };
+
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.setState(state => ({ expanded: !state.expanded }));
+  }
+
+  render() {
+    return (
+      <Example title="Collapsible Dropdown Listbox Example">
+        <div className={styles.container}>
+          <span>Choose an element:</span>
+          <div className={styles.wrapper}>
+            <button
+              aria-expanded={this.state.expanded}
+              aria-haspopup="listbox"
+              className={styles.button}
+              onClick={this.onClick}
+              type="button"
+            >
+              Neptunium
+            </button>
+          </div>
         </div>
-      </div>
-    </Example>
-  );
+      </Example>
+    );
+  }
 }
