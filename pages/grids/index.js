d50b9eaa8bc3ebc13cb7e26a0b38f9b26205798d
Rob Levin <roblevintennis@gmail.com>
2019-07-09 17:01:34 -0700

    Upgrades babel and core-js 3 seems to work fine as well. Removes the babel/polyfill imports for the new way to import core-js and regenerator

--- pages/grids/index.js
+++ pages/grids/index.js
@@ -1,6 +1,7 @@
 /* global window:false */
 
-import '@babel/polyfill'; // eslint-disable-line import/no-extraneous-dependencies
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import React from 'react';
 import { render } from 'react-dom';
 
