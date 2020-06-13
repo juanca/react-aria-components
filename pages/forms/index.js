2a165e70f0861871404dbbbe53075bd8fc0b2a97
Juan Carlos Medina <juanca.med@gmail.com>
2020-06-13 11:36:41 -0700

    Hoist form control examples into separate components

--- pages/forms/index.js
+++ pages/forms/index.js
@@ -9,22 +9,34 @@ import FormSelect from '../../src/form-select/form-select.js';
 import Footer from '../footer.js';
 import Header from '../header.js';
 
+function FormInputExample() {
+  return (
+    <section>
+      <h2>Form Input Example</h2>
+      <FormInput id="example-1" label="This is a label" />
+    </section>
+  );
+}
+
+function FormSelectExample() {
+  return (
+    <section>
+      <h2>Form Select Example</h2>
+      <FormSelect id="example-2" label="This is a label">
+        <option>Default option</option>
+        <option>Another option</option>
+        <option>Hello world!</option>
+      </FormSelect>
+    </section>
+  );
+}
+
 render((
   <React.Fragment>
     <Header link="./forms" title="Forms" />
     <main>
-      <section>
-        <h2>Form Input Example</h2>
-        <FormInput id="example-1" label="This is a label" />
-      </section>
-      <section>
-        <h2>Form Select Example</h2>
-        <FormSelect id="example-2" label="This is a label">
-          <option>Default option</option>
-          <option>Another option</option>
-          <option>Hello world!</option>
-        </FormSelect>
-      </section>
+      <FormInputExample />
+      <FormSelectExample />
     </main>
     <Footer />
   </React.Fragment>
