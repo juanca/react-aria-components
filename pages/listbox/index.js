6edd70afde449424fa0ed379166748709664981a
Juan Carlos Medina <juanca.med@gmail.com>
2020-07-05 11:00:41 -0700

    Implement single select listboxes example

--- pages/listbox/index.js
+++ pages/listbox/index.js
@@ -2,7 +2,9 @@
 
 import '@babel/polyfill'; // eslint-disable-line import/no-extraneous-dependencies
 import React, {
+  createRef,
   useRef,
+  useState,
 } from 'react';
 import { render } from 'react-dom';
 
@@ -47,29 +49,131 @@ function ScrollableListExample() {
     <section>
       <h2>Scrollable Listbox Example</h2>
       <p>
-        <a href="https://www.w3.org/TR/wai-aria-practices/examples/listbox/listbox-scrollable.html">{/* eslint-disable-line max-len */}
+        <a href="https://www.w3.org/TR/wai-aria-practices/examples/listbox/listbox-scrollable.html">
           WAI ARIA practices example source.
         </a>
       </p>
-      <div className={styles['scrollable-example-container']}>
-        <label id="scrollable-list-example-label">
-          Transuranium elements:
-        </label>
-        <Listbox
-          className={styles['scrollable-example-listbox']}
-          labelledBy="scrollable-list-example-label"
-          refs={refs}
-        >
-          {options.map((option, index) => (
-            <ListOption
-              className={styles['scrollable-example-option']}
-              key={option}
-              ref={refs[index]}
-            >
-              {option}
-            </ListOption>
-          ))}
-        </Listbox>
+      <div className={styles['example-container']}>
+        <div>
+          <label id="scrollable-list-example-label">
+            Transuranium elements:
+          </label>
+          <Listbox
+            className={styles['example-listbox']}
+            labelledBy="scrollable-list-example-label"
+            refs={refs}
+          >
+            {() => options.map((option, index) => (
+              <ListOption
+                className={styles['example-option']}
+                key={option}
+                ref={refs[index]}
+                value={option}
+              >
+                {option}
+              </ListOption>
+            ))}
+          </Listbox>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+function SingleSelectListboxExample() {
+  const options = [
+    'Proximity of public K-12 schools',
+    'Proximity of child-friendly parks',
+    'Proximity of grocery shopping',
+    'Proximity of fast food',
+    'Proximity of fine dining',
+    'Neighborhood walkability',
+    'Availability of public transit',
+    'Proximity of hospital and medical services',
+    'Level of traffic noise',
+    'Access to major highways',
+  ];
+
+  const importantListboxRef = useRef();
+  const [importantFeatures, setImportantFeatures] = useState(options);
+  const importantFeaturesRefs = importantFeatures.map(() => createRef());
+  const notImportantListboxRef = useRef();
+  const [notImportantFeatures, setNotImportantFeatures] = useState([]);
+  const notImportantFeaturesRefs = notImportantFeatures.map(() => createRef());
+
+  function onNotImporantClick() {
+    const selectedFeature = importantListboxRef.current.value;
+    setImportantFeatures(importantFeatures.filter(feature => feature !== selectedFeature));
+    setNotImportantFeatures([...notImportantFeatures, selectedFeature]);
+  }
+
+  function onImportantClick() {
+    const selectedFeature = importantListboxRef.current.value;
+    setImportantFeatures([...importantFeatures, selectedFeature]);
+    setNotImportantFeatures(notImportantFeatures.filter(feature => feature !== selectedFeature));
+  }
+
+  return (
+    <section>
+      <h2>Single-Select Listbox Example</h2>
+      <p>
+        <a href="https://www.w3.org/TR/wai-aria-practices/examples/listbox/listbox-rearrangeable.html">
+          WAI ARIA practices examples source.
+        </a>
+      </p>
+      <div className={styles['example-container']}>
+        <div style={{ width: '100%' }}>
+          <label id="single-select-example-label-1">
+            Important Features:
+          </label>
+          <Listbox
+            className={styles['example-listbox']}
+            labelledBy="single-select-example-label-1"
+            ref={importantListboxRef}
+            refs={importantFeaturesRefs}
+          >
+            {({ onSelectChange }) => importantFeatures.map((upgrade, index) => (
+              <ListOption
+                className={styles['example-option']}
+                key={upgrade}
+                onSelectChange={onSelectChange}
+                ref={importantFeaturesRefs[index]}
+                value={upgrade}
+              >
+                {upgrade}
+              </ListOption>
+            ))}
+          </Listbox>
+          <button onClick={onNotImporantClick} type="button">
+            Not Important →
+          </button>
+        </div>
+        <div style={{ marginLeft: '20px', width: '100%' }}>
+          <label id="single-select-example-label-2">
+            Unimportant Features:
+          </label>
+          <Listbox
+            className={styles['example-listbox']}
+            labelledBy="single-select-example-label-2"
+            ref={notImportantListboxRef}
+            refs={notImportantFeaturesRefs}
+          >
+            {({ onSelectChange }) => notImportantFeatures.map((upgrade, index) => (
+              <ListOption
+                className={styles['example-option']}
+                key={upgrade}
+                onSelectChange={onSelectChange}
+                ref={notImportantFeaturesRefs[index]}
+                value={upgrade}
+              >
+                {upgrade}
+              </ListOption>
+            ))}
+          </Listbox>
+          <button onClick={onImportantClick} type="button">
+            ← Important
+          </button>
+        </div>
       </div>
     </section>
   );
@@ -80,6 +184,7 @@ render((
     <Header link="./listbox" title="Listbox" />
     <main>
       <ScrollableListExample />
+      <SingleSelectListboxExample />
     </main>
     <Footer />
   </React.Fragment>
