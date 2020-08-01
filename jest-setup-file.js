81a7d9cc1beaf34d8eea4080e99fb9c6de1c90ae
Juan Carlos Medina <juanca.med@gmail.com>
2020-08-01 15:21:02 -0700

    Update snapshot

--- jest-setup-file.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-console */
-
-console.error = (...args) => {
-  throw new Error([
-    '>>> console.error arguments start',
-    args.join('\n'),
-    '>>> console.error arguments end',
-  ]);
-};
